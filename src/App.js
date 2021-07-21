a0e3e5db5f3c46c9a295258f5d40f591818d0357
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-07-21 12:30:27 +0530

    CREATE: nav component styles added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Nav from "./components/Nav";
 
 function App() {
 	return (
 		<Router>
-			<div className="container px-8 mx-auto">
-				<Nav />
-			</div>
+			<Nav />
 		</Router>
 	);
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,25 +3,27 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
 	return (
-		<header className="flex justify-between items-center h-20">
-			<h3 className="text-2xl uppercase font-medium">Egg</h3>
-			<nav className="w-1/4">
-				<ul className="flex justify-between font-medium text-indigo-700">
-					<li>
-						<Link>Home</Link>
-					</li>
-					<li>
-						<Link>Menu</Link>
-					</li>
-					<li>
-						<Link>About</Link>
-					</li>
-					<li>
-						<Link>Contact</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
+		<div className="min-w-full mx-auto shadow-sm">
+			<header className="container mx-auto flex justify-between items-center h-20">
+				<h3 className="text-2xl uppercase font-medium">Egg</h3>
+				<nav className="w-1/4">
+					<ul className="flex justify-between font-medium text-indigo-700">
+						<li>
+							<Link>Home</Link>
+						</li>
+						<li>
+							<Link>Menu</Link>
+						</li>
+						<li>
+							<Link>About</Link>
+						</li>
+						<li>
+							<Link>Contact</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
 	);
 };
 
